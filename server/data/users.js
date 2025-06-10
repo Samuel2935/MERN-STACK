f92d44b23da173a39b394287d084a9fde5542260
Samuel Ezeh <samuelblessed38@gmail.com>
2025-06-10 23:24:49 +0100

    update

--- server/data/users.js
+++ server/data/users.js
@@ -1,6 +1,7 @@
 let users = [
   { id: "1", name: "Alice", email: "alice@example.com" },
   { id: "2", name: "Bob", email: "bob@example.com" },
+   { id: "3", name: "Sam", email: "sam@example.com" },
 ];
 
 module.exports = users;
