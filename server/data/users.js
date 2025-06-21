3decd3d1dc7f0b9e19a670fa0cece1de526a08bf
Samuel Ezeh <samuelblessed38@gmail.com>
2025-06-21 18:38:18 +0100

    feat: database table

--- server/data/users.js
+++ server/data/users.js
@@ -2,6 +2,7 @@ let users = [
   { id: "1", name: "Alice", email: "alice@example.com" },
   { id: "2", name: "Bob", email: "bob@example.com" },
    { id: "3", name: "Sam", email: "sam@example.com" },
+    { id: "4", name: "Ezeh", email: "ezeh@example.com" },
 ];
 
 module.exports = users;
